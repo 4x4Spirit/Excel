a00209e374796744871349dff270f8d01a2b8f10
phamdung <dung.pham@orange.fr>
2018-03-17 18:17:31 +0100

    First Release

---
+++ CreateExcelWorksheet/CreateExcelWorksheet/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CreateExcelWorksheet
+{
+    partial class FormExcel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createFile = new System.Windows.Forms.Button();
+            this.createRange = new System.Windows.Forms.Button();
+            this.readExcel = new System.Windows.Forms.Button();
+            this.readAll = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.RESET = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // createFile
+            // 
+            this.createFile.Location = new System.Drawing.Point(140, 103);
+            this.createFile.Name = "createFile";
+            this.createFile.Size = new System.Drawing.Size(75, 23);
+            this.createFile.TabIndex = 0;
+            this.createFile.Text = "Create file";
+            this.createFile.UseVisualStyleBackColor = true;
+            this.createFile.Click += new System.EventHandler(this.createFile_Click);
+            // 
+            // createRange
+            // 
+            this.createRange.Location = new System.Drawing.Point(139, 192);
+            this.createRange.Name = "createRange";
+            this.createRange.Size = new System.Drawing.Size(75, 35);
+            this.createRange.TabIndex = 1;
+            this.createRange.Text = "Create Range ";
+            this.createRange.UseVisualStyleBackColor = true;
+            this.createRange.Click += new System.EventHandler(this.createRange_Click);
+            // 
+            // readExcel
+            // 
+            this.readExcel.Location = new System.Drawing.Point(295, 103);
+            this.readExcel.Name = "readExcel";
+            this.readExcel.Size = new System.Drawing.Size(99, 23);
+            this.readExcel.TabIndex = 2;
+            this.readExcel.Text = "Read excel file";
+            this.readExcel.UseVisualStyleBackColor = true;
+            this.readExcel.Click += new System.EventHandler(this.readExcel_Click);
+            // 
+            // readAll
+            // 
+            this.readAll.Location = new System.Drawing.Point(295, 191);
+            this.readAll.Name = "readAll";
+            this.readAll.Size = new System.Drawing.Size(75, 23);
+            this.readAll.TabIndex = 3;
+            this.readAll.Text = "Read all";
+            this.readAll.UseVisualStyleBackColor = true;
+            this.readAll.Click += new System.EventHandler(this.readAll_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(140, 275);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Write ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // RESET
+            // 
+            this.RESET.AutoSize = true;
+            this.RESET.Location = new System.Drawing.Point(220, 381);
+            this.RESET.Name = "RESET";
+            this.RESET.Size = new System.Drawing.Size(43, 13);
+            this.RESET.TabIndex = 5;
+            this.RESET.Text = "RESET";
+            this.RESET.Click += new System.EventHandler(this.RESET_Click);
+            // 
+            // FormExcel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 450);
+            this.Controls.Add(this.RESET);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.readAll);
+            this.Controls.Add(this.readExcel);
+            this.Controls.Add(this.createRange);
+            this.Controls.Add(this.createFile);
+            this.Name = "FormExcel";
+            this.Text = "Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createFile;
+        private System.Windows.Forms.Button createRange;
+        private System.Windows.Forms.Button readExcel;
+        private System.Windows.Forms.Button readAll;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label RESET;
+    }
+}
+
---
+++ CreateExcelWorksheet/CreateExcelWorksheet/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection; 
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace CreateExcelWorksheet
+{
+    public partial class FormExcel : Form
+    {
+        public FormExcel()
+        {
+            InitializeComponent();
+        }
+
+        private void createFile_Click(object sender, EventArgs e)
+        {
+            RESET_Click(sender, e);
+            createFile.BackColor = System.Drawing.Color.Chartreuse;
+            MessageBox.Show("button1 , \nThis will create excel file  : d:\\csharp-Excel.xls");
+            Microsoft.Office.Interop.Excel.Application xlApp = new
+            Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null) 
+            { 
+                MessageBox.Show("Excel is not properly installed!!"); 
+                return;
+            }
+                MessageBox.Show("Excel is installed on this computer");
+
+
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+
+          
+
+
+
+
+            object misValue = System.Reflection.Missing.Value;
+
+                xlWorkBook = xlApp.Workbooks.Add(misValue); //create new Workbook
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1); // Get the worksheet number 1
+                xlWorkSheet.Name = "sheet1";
+
+                xlWorkSheet.Cells[1, 1] = "cell 1 1";                        //write content to worksheet
+                xlWorkSheet.Cells[1, 2] = "cell 1 2";
+                xlWorkSheet.Cells[2, 1] = "cell 2 1";
+                xlWorkSheet.Cells[2, 2] = "cell 2 2";
+
+
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.Add(System.Reflection.Missing.Value,xlWorkBook.Worksheets[xlWorkBook.Worksheets.Count],System.Reflection.Missing.Value,System.Reflection.Missing.Value); // Create new worksheet
+                xlWorkSheet.Name = "sheet2";
+
+                xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.Add(System.Reflection.Missing.Value, xlWorkBook.Worksheets[xlWorkBook.Worksheets.Count], System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+                xlWorkSheet.Name = "sheet3";
+
+
+
+                xlWorkBook.SaveAs("d:\\csharp-Excel.xls",
+                    Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+                    Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue); //save the excel file.
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlApp);
+
+                MessageBox.Show("Excel file created , you can find the file d:\\csharp-Excel.xls");
+            
+        }
+
+        private void createRange_Click(object sender, EventArgs e)
+        {
+            //This will create a Excel app and write on this 
+
+            RESET_Click(sender, e);
+            createRange.BackColor = System.Drawing.Color.Chartreuse;
+                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+                if (xlApp == null)
+                {
+                    MessageBox.Show("EXCEL could not be started. Check that your office installation and project references are correct.");
+                    return;
+                }
+                xlApp.Visible = true;
+
+                Workbook wb = xlApp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+                Worksheet ws = (Worksheet)wb.Worksheets[1];
+                ws.Name = "page1";
+
+
+
+            if (ws == null)
+            {
+                MessageBox.Show("Worksheet could not be created. Check that your office installation and project references are correct.");
+            }
+
+            else
+            {
+                // Select the Excel cells, in the range c1 to c7 in the worksheet.
+                Range aRange = ws.get_Range("C1", "C7");
+
+                if (aRange == null)
+                {
+                    MessageBox.Show("Could not get a range. Check to be sure you have the correct versions of the office DLLs.");
+                }
+
+                // Fill the cells in the C1 to C7 range of the worksheet with the number 6.
+                Object[] args = new Object[1];
+                args[0] = 6;
+
+                aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+
+                // Change the cells in the C1 to C7 range of the worksheet to the number 8.
+                aRange.Value2 = 8;
+
+
+
+                aRange = ws.get_Range("A1", "B7");
+                args[0] = 6;
+                aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+                aRange.Value2 = 5;
+
+                // Create workshit page2
+                Excel.Worksheet addedSheet = wb.Worksheets.Add(Type.Missing,wb.Worksheets[1], Type.Missing, Type.Missing);
+                addedSheet.Name = "page2";
+
+                addedSheet.Cells[2, 3] = "here is rox2 - column3";
+
+                // Create workshit page3
+                int count = wb.Worksheets.Count;
+                addedSheet = wb.Worksheets.Add(Type.Missing, wb.Worksheets[count], Type.Missing, Type.Missing);
+                addedSheet.Name = "page3";
+                ws = (Worksheet)wb.Worksheets[3];
+                aRange = ws.get_Range("B1", "B7");
+                args[0] = 6;
+                aRange.GetType().InvokeMember("Value", BindingFlags.SetProperty, null, aRange, args);
+                aRange.Value2 = "page3";
+
+                xlApp.ActiveWorkbook.Sheets[1].Select(); //activates first sheet
+            }
+
+            //wb.Close(true, null, null);
+            //xlApp.Quit();
+            releaseObject(ws);
+            releaseObject(wb);
+            releaseObject(xlApp);
+
+        }
+
+
+
+        private void readExcel_Click(object sender, EventArgs e)
+        {
+            RESET_Click(sender, e);
+            readExcel.BackColor = System.Drawing.Color.Chartreuse;
+            Microsoft.Office.Interop.Excel.Application xlApp;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open("d:\\csharp-Excel.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            Range aRange = xlWorkSheet.get_Range("A1", "A2");
+
+            if (aRange == null)
+            {
+                MessageBox.Show("Could not get a range. Check to be sure you have the correct versions of the office DLLs.");
+            }
+            else
+            {
+                MessageBox.Show(xlWorkSheet.get_Range("A1", "A1").Value2.ToString());
+                MessageBox.Show(xlWorkSheet.get_Range("A2", "A2").Value2.ToString());
+
+            }
+
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void readAll_Click(object sender, EventArgs e)
+        {
+            RESET_Click(sender, e);
+            readAll.BackColor = System.Drawing.Color.Chartreuse;
+
+            Microsoft.Office.Interop.Excel.Application xlRead;
+            Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+            Microsoft.Office.Interop.Excel.Range range;
+
+            string str;
+            int rCnt = 0;
+            int cCnt = 0;
+
+            xlRead = new Microsoft.Office.Interop.Excel.Application();
+            xlWorkBook = xlRead.Workbooks.Open("d:\\csharp-Excel.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0); //WorkBooks.open(string Filename, object UpdateLinks, object ReadOnly, object Format, object Password, object WriteResPassword, object ReadOnlyRecommend, object Origin, object Delimiter, object Editable, object Notify, object Converter, object AddToMru, object Local, object CorruptLoad )
+            xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+
+            for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+                {
+                    str = (string)(range.Cells[rCnt, cCnt] as Microsoft.Office.Interop.Excel.Range).Value2;
+                    MessageBox.Show("Worksheet name : "  + xlWorkSheet.Name  + "\n" +  str);
+                }
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlRead.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlRead);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            RESET_Click(sender, e);
+
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel.Workbook g_Workbook = excelApp.Workbooks.Open(
+                @"d:\\csharp-Excel.xls",
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int count = g_Workbook.Worksheets.Count;
+                MessageBox.Show("" + count);
+                Excel.Worksheet addedSheet = g_Workbook.Worksheets.Add(Type.Missing,g_Workbook.Worksheets[count], Type.Missing, Type.Missing);
+                addedSheet.Name = i.ToString();
+            }
+        }
+
+        private void RESET_Click(object sender, EventArgs e)
+        {
+            createFile.BackColor = System.Drawing.SystemColors.Control;
+            createRange.BackColor = System.Drawing.SystemColors.Control;
+            readExcel.BackColor = System.Drawing.SystemColors.Control;
+            readAll.BackColor = System.Drawing.SystemColors.Control;
+        }
+    }
+}
